7f4a6a50547985f4c1ac0a59cdb2ec1417ad4d2d
Vincenzo Greggio <vincenzogreggio@hotmail.com>
2016-05-31 05:11:29 -0500

    Primer commit, varias pruebas =D

---
+++ ProbandoJenkins/Prueba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbandoJenkins
+{
+    public class Prueba
+    {
+        public int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string Unir(string a, string b)
+        {
+            if (a == null || b == null) return null;
+            return a + b;
+        }
+
+        public bool CompararIguales(string a, string b)
+        {
+            if (a == b) return true;
+            return false;
+        }
+    }
+}
